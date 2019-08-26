e0ce5ae2e71b6912c30dbe256cd33cdd60f25d29
Mark Rego <rego121@gmail.com>
2019-08-26 18:07:26 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,20 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+class show
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
 
-  def initialize(brand)
-    @brand = brand
+  GENRES = []
+
+  def initialize(title)
+    @title = title
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
